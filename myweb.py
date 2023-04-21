24f18836f46122e3c9b396fc90df7e1561f80f5c
IDRIS <eedriss67@gmail.com>
2023-04-21 01:26:22 +0100

    edited

--- myweb.py
+++ myweb.py
@@ -116,11 +116,11 @@ class Job(db.Model):
 # Custom class for the Admin View
 class AdminView(AdminIndexView):
     def is_accessible(self):
-        if not current_user:
-            return redirect(url_for('login'))
+        if current_user.is_authenticated:
+            return redirect(url_for('admin'))
 
         else:
-            return redirect(url_for('admin'))
+            return redirect(url_for('login'))
 
 
 
