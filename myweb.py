4cefbe0fd5de6798654929212114631bfa94ae68
IDRIS <eedriss67@gmail.com>
2023-04-22 16:45:25 +0100

    edited

--- myweb.py
+++ myweb.py
@@ -1,6 +1,7 @@
 from flask import Flask, Blueprint, render_template, request, redirect, url_for, flash, session, logging, abort
 from flask_login import LoginManager, login_user, logout_user, current_user, login_required, UserMixin
 from forms import RegistrationForm, ContactForm, LoginForm
+from config import EMAIL, PASSWORD, SENDER
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
 from flask_mail import Mail, Message
@@ -44,7 +45,6 @@ with app.app_context():
 login_manager = LoginManager(app)
 
 
-from config import EMAIL, PASSWORD, SENDER
 
 # Configure Flask-Mail
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
