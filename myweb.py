bbf3a0ce452ac2ac6452a1714f4f88cf893a9a06
IDRIS <eedriss67@gmail.com>
2023-04-23 15:27:23 +0100

    edited

--- myweb.py
+++ myweb.py
@@ -50,9 +50,9 @@ login_manager = LoginManager(app)
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
 app.config['MAIL_USE_SSL'] = True
-app.config['MAIL_USERNAME'] = EMAIL
-app.config['MAIL_PASSWORD'] = PASSWORD
-app.config['MAIL_DEFAULT_SENDER'] = SENDER
+app.config['MAIL_USERNAME'] = settings.EMAIL
+app.config['MAIL_PASSWORD'] = settings.PASSWORD
+app.config['MAIL_DEFAULT_SENDER'] = settings.SENDER
 
 mail = Mail(app)
 
@@ -189,7 +189,7 @@ def contact():
         message = form.message.data
 
         # Send the email
-        msg = Message(subject, recipients=[EMAIL])
+        msg = Message(subject, recipients=[settings.EMAIL])
         msg.body = f"Name: {name}\nEmail: {email}\n\n{message}"
         mail.send(msg)
 
