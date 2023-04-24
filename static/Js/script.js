103e950e7f9fd758b1b05b6274836cc665a45ab1
IDRIS <eedriss67@gmail.com>
2023-04-24 22:29:32 +0100

    edited

--- static/Js/script.js
+++ static/Js/script.js
@@ -1,6 +1,59 @@
 /* Java Script for the Carousel slider */
-
 const carousel = document.querySelector('.carousel');
+const carouselImages = document.querySelector('.carousel-images');
+const carouselPrev = document.querySelector('.prev');
+const carouselNext = document.querySelector('.next');
+
+let currentSlide = 0;
+const totalSlides = document.querySelectorAll('.carousel-images img').length;
+
+function nextSlide() {
+  currentSlide++;
+  carouselImages.style.transition = 'transform 0.4s ease-in-out';
+  carouselImages.style.transform = `translateX(-${currentSlide * 25}%)`;
+
+  // Reset to first slide if we've reached the end of the carousel
+  if (currentSlide >= totalSlides) {
+    setTimeout(() => {
+      carouselImages.style.transition = 'none';
+      carouselImages.style.transform = 'translateX(0)';
+      currentSlide = 0;
+    });
+  }
+}
+
+function prevSlide() {
+  currentSlide--;
+  carouselImages.style.transition = 'transform 0.4s ease-in-out';
+  carouselImages.style.transform = `translateX(-${currentSlide * 25}%)`;
+
+  // Move to last slide if we've reached the beginning of the carousel
+  if (currentSlide <= totalSlides) {
+    setTimeout(() => {
+      carouselImages.style.transition = 'none';
+      carouselImages.style.transform = `translateX(-${currentSlide  * 25}%)`;
+      currentSlide = totalSlides;
+    });
+  }
+}
+
+// Set the time interval (in milliseconds)
+const intervalTime = 1500; // 1.5 seconds
+
+// Call the nextSlide function at the specified interval
+setInterval(nextSlide, intervalTime);
+
+carouselNext.addEventListener('click', nextSlide);
+carouselPrev.addEventListener('click', prevSlide);
+
+
+
+
+
+
+
+
+/*const carousel = document.querySelector('.carousel');
 const carouselImages = document.querySelector('.carousel-images');
 const carouselPrev = document.querySelector('.prev');
 const carouselNext = document.querySelector('.next');
@@ -24,13 +77,13 @@ function prevSlide() {
 }
 
 // Set the time interval (in milliseconds)
-const intervalTime = 5000; // 5 seconds
+const intervalTime = 1500; // 1.5 seconds
 
 // Call the nextSlide function at the specified interval
 setInterval(nextSlide, intervalTime);
 
 carouselNext.addEventListener('click', nextSlide);
-carouselPrev.addEventListener('click', prevSlide);
+carouselPrev.addEventListener('click', prevSlide);*/
 
 
 
@@ -38,21 +91,20 @@ carouselPrev.addEventListener('click', prevSlide);
 
 
 /*Back to top button events Listener*/
+var btn = document.querySelector('.back-to-top');
 
-  var btn = document.querySelector('.back-to-top');
+window.addEventListener('scroll', function() {
+  if (window.pageYOffset > 300) {
+    btn.style.display = 'block';
+  } else {
+    btn.style.display = 'none';
+  }
+});
 
-  window.addEventListener('scroll', function() {
-    if (window.pageYOffset > 300) {
-      btn.style.display = 'block';
-    } else {
-      btn.style.display = 'none';
-    }
-  });
-
-  btn.addEventListener('click', function(e) {
-    e.preventDefault();
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-  });
+btn.addEventListener('click', function(e) {
+  e.preventDefault();
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+});
 
 
 
